bab34dd6e03acfb8be0dfc0ad8d7de5facd761b8
Jan Pierry <janpierrycoelho@gmail.com>
2018-06-13 03:08:06 -0300

    versão provisória

--- src/trabalho2/Cliente.java
+++ src/trabalho2/Cliente.java
@@ -6,9 +6,35 @@
 package trabalho2;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.PrintStream;
+import java.net.ServerSocket;
 import java.net.Socket;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.Security;
 import java.util.Scanner;
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
+import org.bouncycastle.crypto.InvalidCipherTextException;
+import org.bouncycastle.crypto.engines.AESEngine;
+import org.bouncycastle.crypto.modes.GCMBlockCipher;
+import org.bouncycastle.crypto.params.AEADParameters;
+import org.bouncycastle.crypto.params.KeyParameter;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 /**
  *
@@ -16,12 +42,14 @@ import java.util.Scanner;
  */
 public class Cliente {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) throws IOException {
+    private static Socket socket = null;
+    
+    private static SecretKey chavesessao = null;
+    private static byte[] iv = null;
+    
+    public static void main(String[] args) throws IOException, Exception {
 
-        Socket cliente = new Socket("127.0.0.1", 12345);
+        /*Socket cliente = new Socket("127.0.0.1", 12345);
 
         System.out.println("O cliente se conectou ao servidor!");
 
@@ -37,8 +65,112 @@ public class Cliente {
 
         saida.close();
 
-        teclado.close();
+        teclado.close();*/
         
+        estabeleceConexao();
+        
+        enviaMsgServidor();
+        
+    }
+    
+    private static void estabeleceConexao() throws IOException, Exception {
+
+        socket = new Socket("127.0.0.1", 12345);
+        System.out.println("Conexão estabelecida com o servidor!");
+        
+        //Pergunta a senha para ser derivada a chave de sessão
+        System.out.println("Insira a senha que será utilizada para derivar sua chave de sessão: ");
+        Scanner scanner = new Scanner(System.in);
+        chavesessao = generateDerivedKey(scanner.nextLine(), "881900f5d6e5cabca409675791601323", 10000);
+        
+        //Gera iv/nonce
+        iv = geraIV();
+        
+        PrintStream saida = new PrintStream(socket.getOutputStream());
+        saida.println("teste");
+        System.out.println("Enviado para o servidor: teste");
+        
+        
+        
+        Scanner entrada = new Scanner(socket.getInputStream());
+        
+        if(entrada.hasNextLine()) {
+
+            System.out.println("Recebido do servidor: " + entrada.nextLine());
+
+        }
+        
+        saida.println("uhull");
+        System.out.println("Enviado para o servidor: uhull");
+        
+        //entrada.close();
+        
+        
+    }
+
+    private static void enviaMsgServidor() throws IOException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException, DecoderException, IllegalStateException, InvalidCipherTextException {
+
+        int addProvider1 = Security.addProvider(new BouncyCastleProvider());
+        
+        Scanner teclado = new Scanner(System.in);
+
+        PrintStream saida = new PrintStream(socket.getOutputStream());
+        
+        IvParameterSpec ivSpec = new IvParameterSpec(iv);
+
+        while (teclado.hasNextLine()) {
+
+            /*
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            KeyGenerator sKenGen = KeyGenerator.getInstance("AES");
+            Key aesKey = sKenGen.generateKey();
+            */
+            GCMBlockCipher gcmChave = new GCMBlockCipher(new AESEngine());
+            String chaveHex = Hex.encodeHexString(chavesessao.getEncoded());
+            byte[] chave = org.apache.commons.codec.binary.Hex.decodeHex(chaveHex.toCharArray());
+            KeyParameter chave2 = new KeyParameter(chave);
+            AEADParameters params = new AEADParameters(chave2, 64, iv);
+            
+            gcmChave.init(true, params);
+            
+            byte[] msgBytes = teclado.nextLine().getBytes();
+            int outsize = gcmChave.getOutputSize(msgBytes.length);
+            byte[] msgCifradaBytes = new byte[outsize];
+            
+            int lengthOutc = gcmChave.processBytes(msgBytes, 0, msgBytes.length, msgCifradaBytes, 0);
+        
+            gcmChave.doFinal(msgCifradaBytes, lengthOutc);
+            
+            String msgCifradaHex = Utils4.toHex(msgCifradaBytes);
+            System.out.println("Mensagem cifrada enviada:" + msgCifradaHex);
+            saida.println(msgCifradaHex);
+
+        }
+        
+    }
+    
+    public static SecretKey generateDerivedKey(String password, String salt, Integer iterations) {
+        PBEKeySpec spec = new PBEKeySpec(password.toCharArray(), salt.getBytes(), iterations, 128);
+        SecretKeyFactory pbkdf2 = null;
+        String derivedPass = null;
+        try {
+            pbkdf2 = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+            SecretKey sk = pbkdf2.generateSecret(spec);
+            return sk;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    
+    public static byte[] geraIV()throws Exception{
+        byte[] iv = new byte[16];
+        SecureRandom random = null;
+        
+        random = SecureRandom.getInstance("SHA1PRNG", "SUN");
+        
+        random.nextBytes(iv);
+        return iv;
     }
     
 }
--- src/trabalho2/Servidor.java
+++ src/trabalho2/Servidor.java
@@ -6,9 +6,16 @@
 package trabalho2;
 
 import java.io.IOException;
+import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.security.Key;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.Security;
 import java.util.Scanner;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 /**
  *
@@ -16,12 +23,14 @@ import java.util.Scanner;
  */
 public class Servidor {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) throws IOException {
+    private static Socket socket = null;
+    
+    private static Key chavePublica = null;
+    private static Key chavePrivada = null;
+    
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
 
-        ServerSocket servidor = new ServerSocket(12345);
+        /*ServerSocket servidor = new ServerSocket(12345);
 
         System.out.println("Porta 12345 aberta!");
 
@@ -40,7 +49,72 @@ public class Servidor {
 
         entrada.close();
 
-        servidor.close();
+        servidor.close();*/
+        
+        geraChavePubPriv();
+        
+        estabeleceConexao();
+        
+        ouvirCliente();
+        
+        
+        
+    }
+    
+    public static void estabeleceConexao() throws IOException{
+        
+        ServerSocket servidor = new ServerSocket(12345);
+        
+        System.out.println("Aguardando o cliente...");
+        socket = servidor.accept();
+        System.out.println("Conexão estabelecida com o cliente!");
+        
+        Scanner entrada = new Scanner(socket.getInputStream());
+        
+        if(entrada.hasNextLine()) {
+
+            System.out.println("Recebido do cliente: " + entrada.nextLine());
+
+        }
+        
+        PrintStream saida = new PrintStream(socket.getOutputStream());
+        
+        saida.println("funcionou?");
+        System.out.println("Enviado para o cliente: funcionou?");
+        
+        if(entrada.hasNextLine()) {
+
+            System.out.println("Recebido do cliente: " + entrada.nextLine());
+
+        }
+        
+        //entrada.close();
+        //saida.close();
+        
+    }
+
+    private static void ouvirCliente() throws IOException {
+        
+        int addProvider1 = Security.addProvider(new BouncyCastleProvider());
+
+        Scanner entrada = new Scanner(socket.getInputStream());
+
+        while (entrada.hasNextLine()) {
+
+            //Falta decifrar a mensagem cifrada do cliente
+            System.out.println(entrada.nextLine());
+
+        }
+        
+    }
+
+    private static void geraChavePubPriv() throws NoSuchAlgorithmException {
+
+        KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
+        
+        KeyPair pair = generator.generateKeyPair();
+        chavePublica = pair.getPublic();
+        chavePrivada = pair.getPrivate();
         
     }
     
